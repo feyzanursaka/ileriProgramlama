af6502900517cebea2a65ad416f57406c0e6c4fe
AyseSenaFeyiz <36419031+AyseSenaFeyiz@users.noreply.github.com>
2018-10-24 16:37:00 +0300

    Update Database.js

--- Database.js
+++ Database.js
@@ -1,67 +1,70 @@
 class Course {
-  constructor(name, time, date, rooms) {
-    this.name = name; 
-    this.time = time;
-    this.date = date;
-    this.rooms = rooms;
-  }
-  toString() {
-    return “Name = ”+ this.name; 
-  }
+    constructor (name, time, date, rooms) {
+        this.name = name;
+        this.time = time;
+        this.date = date;
+        this.rooms = rooms;
+    }
+    toString () {
+        return this.name;
+    }
 }
 class Student {
-  constructor(id, name, gpa, courses) {
+    constructor (id, name, gpa, courses) {
         this.id = id;
-	this.name = name; 
-	this.gpa = gpa;
-	this.courses = courses;
-  }
-  toString() {
-    return “Name = ”+ this.name+ " ID = "+ this.id ; 
-  }
+        this.name = name;
+        this.gpa = gpa;
+        this.courses = courses;
+    }
+    toString () {
+        return this.id+'';
+    }
 }
-  class Point {
-  constructor(x, y) {
-    this.x = x; 
-    this.y = y;
-  }
-  toString() {
-    return “ x = ”+ this.x+ “ y = ”+this.y; 
-  }
+class Point {
+    constructor (x,y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString () {
+        return '(' +this.x + ', ' + this.y+ ')';
+    }
 }
 class Point3D extends Point {
-constructor(x,y,z){
-super(x,y);
-this.z=z;
-  }
-  toString() {
-        return “ x = ”+ this.x+ “ y = ”+this.y + “ z = ”+this.z;
-  }
+    constructor (x, y, z) {
+        super(x,y);
+        this.z = z;
+    }
+    toString () {
+        return '(' + this.x + ', ' + this.y + ', ' + this.z + ')';
+    }
 }
 class Distance {
-	  constructor(km =0) {
-	    this.km = Math.round(km);
-	  }
-	  get miles() {
-	    return Math.round(this.km /1000);
-	  }
-	  set miles(value) {
-	    this.km =Math.round(value*1000);
-	  }
-	  toString() {
-	    return this.km +"km";
-	  }
-	  static fromMiles (value) {
-	    let t = new Distance();
-	    t.miles = value;
-	    return t;
-	  }
-	}
+    constructor (km = 0) {
+        this.km = Math.round(km);
+    }
+    get miles () {
+        return Math.round(this.km * 1.6);
+    }
+    set miles (value = 0) {
+        this.km = Math.round(value / 1.6);
+    }
+    toString () {
+        return this.km + " km"; 
+    }
+    static fromMiles(value) {
+        let d = new Distance();
+        d.miles = value;
+        return t;
+    }
+}
 class CW4 extends Menu {
-constructor() {
-super();
-this.point=new Point(3,5);
-this.point3D=new Point3D(1,2,3);
-this.km = new Distance(100);
-	  }
-	  }
+    constructor () {
+        super()
+        var ders = new Course("Adv Prog", "15:00", new Date("2018-10-24"), ["B121"]);
+        this.student = new Student(1015, "Bedirhan Yıldırım", 2.0, ders);
+        this.course = ders;
+        this.distance = new Distance(300);
+        this.point = new Point(3,4);
+        this.point3d = new Point3D(3,4,5);
+    }
+}
